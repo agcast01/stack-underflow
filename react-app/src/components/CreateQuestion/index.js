449a36958bc657d1a011e0c1667a69587409330b
Andrew Duong <itsandrewduong@gmail.com>
2023-01-18 16:37:30 -0800

    Added validations

--- react-app/src/components/CreateQuestion/index.js
+++ react-app/src/components/CreateQuestion/index.js
@@ -1,55 +1,70 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { createQuestion } from "../../store/question"
 import { useHistory } from "react-router"
 
-function CreateQuestion(){
+function CreateQuestion() {
   const dispatch = useDispatch()
   const history = useHistory()
   const user = useSelector(state => state.session.user)
   const [text, setText] = useState("")
   const [title, setTitle] = useState('')
+  const [validationErrors, setValidationErrors] = useState([])
 
+  useEffect(() => {
+    const errors = []
+
+    if (!text) errors.push("Question field is required")
+    if (!title) errors.push("Title field is required")
+    if (title.length > 30) errors.push("Title field must be less than 30 characters")
+
+    setValidationErrors(errors)
+  }, [text, title])
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    let error = []
 
-    const payload = {
-      question: text,
-      userId: user.id,
-      title 
+    if (!validationErrors.length) {
+      const payload = {
+        question: text,
+        userId: user.id,
+        title
+      }
+
+      let newQuestion = await dispatch(createQuestion(payload))
+      history.push(`/questions/${newQuestion.id}`)
     }
-
-    let newQuestion = await dispatch(createQuestion(payload))
-    history.push(`/questions/${newQuestion.id}`)
-
   }
 
   return (
-   <div>
-    <form onSubmit = {handleSubmit} className='form'>
-      <div>
-      <label htmlFor="title">Title</label>
-      <input
-      name='title'
-      type="text"
-      value={title}
-      onChange={(e) => setTitle(e.target.value)}
-      />
-      </div>
-      <div>
-        <label htmlFor="question">Question</label>
-      <textarea
-      name="question"
-      value={text}
-      onChange={(e) => setText(e.target.value)}
-      className='textarea'
-      />
-      </div>
-      <button disabled={user === null}>Ask</button>
-    </form>
-   </div>
+    <div>
+      <form onSubmit={handleSubmit} className='form'>
+        <ul className="errors">
+          {validationErrors.length > 0 && validationErrors.map((error, idx) => (
+            <li key={idx}><i class="fa-sharp fa-solid fa-circle-exclamation"></i> {error}</li>
+          ))}
+        </ul>
+        <div>
+          <label htmlFor="title">Title</label>
+          <input
+            name='title'
+            type="text"
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="question">Question</label>
+          <textarea
+            name="question"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            className='textarea'
+          />
+        </div>
+        <button disabled={user === null}>Ask</button>
+      </form>
+    </div>
   )
 }
 
--- react-app/src/components/UpdateProfile/index.js
+++ react-app/src/components/UpdateProfile/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { updateProfile } from "../../store/user"
 import { useHistory, useParams } from "react-router"
@@ -10,29 +10,51 @@ const UpdateProfile = () => {
 
   const user = useSelector(state => state.session.user)
 
-  const [location, setLocation] = useState(user.location)
-  const [title, setTitle] = useState(user.title)
-  const [about, setAbout] = useState(user.about_me)
-  const [website, setWebsite] = useState(user.website_url)
-  const [twitter, setTwitter] = useState(user.twitter_url)
-  const [github, setGithub] = useState(user.github_url)
+  const [location, setLocation] = useState(user.location || "")
+  const [title, setTitle] = useState(user.title || "")
+  const [about, setAbout] = useState(user.about_me || "")
+  const [website, setWebsite] = useState(user.website_url || "")
+  const [twitter, setTwitter] = useState(user.twitter_url || "")
+  const [github, setGithub] = useState(user.github_url || "")
+  const [validationErrors, setValidationErrors] = useState([])
+
+  useEffect(() => {
+    const errors = []
+
+    if (location.length > 30) errors.push("Location field must be less than 30 characters")
+    if (title.length > 30) errors.push("Title field must be less than 30 characters")
+    if (about.length > 255) errors.push("About me field must be less than 255 characters")
+    if (website.length > 50) errors.push("Website url field must be less than 50 characters")
+    if (twitter.length > 50) errors.push("Twitter url field must be less than 50 characters")
+    if (github.length > 50) errors.push("Github url field must be less than 50 characters")
+
+    setValidationErrors(errors)
+  }, [location, title, about, website, twitter, github])
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    user.location = location
-    user.title = title
-    user.about_me = about
-    user.website_url = website
-    user.twitter_url = twitter
-    user.github_url = github
 
-    await dispatch(updateProfile(user, userId))
-    history.push(`/users/${userId}`)
+    if (!validationErrors.length) {
+      user.location = location
+      user.title = title
+      user.about_me = about
+      user.website_url = website
+      user.twitter_url = twitter
+      user.github_url = github
+
+      await dispatch(updateProfile(user, userId))
+      history.push(`/users/${userId}`)
+    }
   }
 
   return (
     <div>
       <form onSubmit={handleSubmit} className='form'>
+        <ul className="errors">
+          {validationErrors.length > 0 && validationErrors.map((error, idx) => (
+            <li key={idx}><i class="fa-sharp fa-solid fa-circle-exclamation"></i> {error}</li>
+          ))}
+        </ul>
         <div>
           <label>Location</label>
           <input
@@ -80,12 +102,12 @@ const UpdateProfile = () => {
         </div>
         <div>
           <label>Github Url</label>
-        
-        <input
-          type='url'
-          value={github}
-          onChange={e => setGithub(e.target.value)}
-        />
+
+          <input
+            type='url'
+            value={github}
+            onChange={e => setGithub(e.target.value)}
+          />
         </div>
         <button disabled={user === null}>Update Profile</button>
       </form>
