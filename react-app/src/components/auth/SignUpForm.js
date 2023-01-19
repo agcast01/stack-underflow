4ae8419335fbd9b67f85b5c6e408fc5bb1174635
Andrew Duong <itsandrewduong@gmail.com>
2023-01-19 13:59:09 -0800

    Added validations to sign up form

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
 import { Redirect } from 'react-router-dom';
 import { signUp } from '../../store/session';
@@ -12,14 +12,27 @@ const SignUpForm = () => {
   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
 
+  const error = []
+
+  // useEffect(() => {
+  //   const errors = []
+
+  //   if (!email.includes('@')) errors.push('Valid email is required')
+
+  //   setErrors(errors)
+  // }, [email])
+
   const onSignUp = async (e) => {
     e.preventDefault();
-    if (password === repeatPassword) {
-      const data = await dispatch(signUp(username, email, password));
-      if (data) {
-        setErrors(data)
-      }
+    if (!email.includes('@')) error.push('Valid email is required')
+    if (password !== repeatPassword) error.push('Confirm password field must be the same as the password field')
+    if (error.length) return setErrors(error)
+
+    const data = await dispatch(signUp(username, email, password));
+    if (data) {
+      setErrors(data)
     }
+    return data
   };
 
   const updateUsername = (e) => {
