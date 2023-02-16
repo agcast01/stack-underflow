3d45aa42e453c2e9b004f847c0e7b6c3ebd95e1f
agcast01 <agcast01@gmail.com>
2023-02-16 16:04:00 -0500

    disabled when not on questions page

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { NavLink, useHistory } from 'react-router-dom';
+import { NavLink, useHistory, useLocation } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
 import QuestionSearch from '../QuestionSearch';
 import { useSelector } from 'react-redux';
@@ -9,6 +9,7 @@ import '../components/auth/auth.css'
 const NavBar = ({ location, setLocation }) => {
   const user = useSelector(state => state.session.user)
   const history = useHistory();
+  const path = useLocation()
   function checkUser() {
     if (user === null) {
       return (
@@ -41,7 +42,7 @@ const NavBar = ({ location, setLocation }) => {
           <img className='logo' src='https://i.imgur.com/Lqj9luv.png' alt='logo' />
         </NavLink>
 
-        <QuestionSearch />
+        {path.pathname === '/' && <QuestionSearch />}
         {checkUser()}
       </nav>
     </div>
